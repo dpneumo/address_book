878f33a41a5854fd35fbcecd6e078ab7d95a2863
Mitch Kupinger <dpneumo@gmail.com>
2024-11-27 19:40:29 -0600

    Make the links a little more friendly and obvious. Improve validation of the State field and normalize the state abreviations used there.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
+STATEABBREV=%w( AL AK AS AZ AR CA CO CT DE DC FM FL GA GU HI ID IL IN IA KS KY LA ME MH MD MA MI MN MS MO MT NE NV NH NJ NM NY ND MP OH OK OR PW PA PR RI SC SD TN TX UT VT VI VA WA WV WI WY )
+
 class Person < ApplicationRecord
+  before_validation :upcase_state
+
   validates :addressee, presence: true
   validates :street,  presence: true
   validates :city,  presence: true
   validates :state,  presence: true
+  validates :state, inclusion: { in: STATEABBREV, message: "%{value} is not a valid state code" }
   validates :zip,  presence: true
+
+  def upcase_state
+    state.strip!.upcase!
+  end
 
   def complete_address
     "#{street}, #{city}, #{state} #{zip}"
@@ -24,4 +33,5 @@ class Person < ApplicationRecord
     return '' unless note
     note.length > 15 ? note.slice(0..14)+'...' : note
   end
+
 end
