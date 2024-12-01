8b976fa815f9f2b93700bbcfefcd6a0f90d73b5a
Mitch Kupinger <dpneumo@gmail.com>
2024-11-30 21:00:12 -0600

    Label output appears to work.

--- app/controllers/avery_controller.rb
+++ app/controllers/avery_controller.rb
@@ -3,6 +3,16 @@ class AveryController < ApplicationController
   end
 
   def labels
-    @people = Person.all
+    @noheader = true
+    @mylabels = label_data
+  end
+
+  def label_data
+    people = Person.all
+    people.map do |person|
+      { addressee: person.label_addressee,
+        street: person.label_street,
+        csz: person.label_csz }
+    end
   end
 end
