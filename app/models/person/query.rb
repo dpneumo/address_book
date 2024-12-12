df6dea006d7f69c6091d77c3a947672ccd8031b2
Mitch Kupinger <dpneumo@gmail.com>
2024-12-12 00:07:15 -0600

    Only define STATEABBREV once

--- app/models/person/query.rb
+++ app/models/person/query.rb
@@ -1,5 +1,3 @@
-STATEABBREV=%w( AL AK AS AZ AR CA CO CT DE DC FM FL GA GU HI ID IL IN IA KS KY LA ME MH MD MA MI MN MS MO MT NC NE NV NH NJ NM NY ND MP OH OK OR PW PA PR RI SC SD TN TX UT VT VI VA WA WV WI WY )
-
 class Person::Query
   include ActiveModel::Model
   include ActiveModel::Attributes
